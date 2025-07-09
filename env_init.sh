cb04a8f4616606d59c60a738372ccc996ffbc9a9
arkanharyo <arkanharyo@gmail.com>
2025-07-09 15:18:58 +0700

    deploy: remove firebase env

--- env_init.sh
+++ env_init.sh
@@ -6,13 +6,5 @@ echo "{
   \"keycloakRealm\": \"${VITE_KEYCLOAK_REALM}\",
   \"keycloakRedirectUri\": \"${VITE_KEYCLOAK_REDIRECT_URI}\",
   \"keycloakClientID\": \"${VITE_KEYCLOAK_CLIENT_ID}\",
-  \"firebaseApiKey\": \"${VITE_FIREBASE_API_KEY}\",
-  \"firebaseAuthDomain\": \"${VITE_FIREBASE_AUTH_DOMAIN}\",
-  \"firebaseProjectId\": \"${VITE_FIREBASE_PROJECT_ID}\",
-  \"firebaseStorageBucket\": \"${VITE_FIREBASE_STORAGE_BUCKET}\",
-  \"firebaseMessagingSenderId\": \"${VITE_FIREBASE_MESSAGING_SENDER_ID}\",
-  \"firebaseAppId\": \"${VITE_FIREBASE_APP_ID}\",
-  \"firebaseMeasurementId\": \"${VITE_FIREBASE_MEASUREMENT_ID}\",
-  \"firebaseVapidKey\": \"${VITE_FIREBASE_VAPID_KEY}\",
   \"testEnv\": \"${VITE_TEST_ENV}\"
 }" > /usr/share/nginx/html/config.json
